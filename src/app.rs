5080a5e4c2d223a93b7dc2265b8ccdc4a7f9f6a6
Abe Massry <a@abemassry.com>
2025-07-08 22:12:09 -0400

    got tabs working

--- src/app.rs
+++ src/app.rs
@@ -3,40 +3,58 @@ use egui_commonmark::*;
 use std::error::Error;
 use pulldown_cmark::{Parser, Options};
 
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Default)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[derive(Clone)]
+struct Tab {
+    // Example stuff:
+    label: String,
+    location: String,
+    status: String,
+
+    // currently loaded page of tab
+    contents: String,
+
+    // for history
+    back: Vec<String>,
+    forward: Vec<String>,
+}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-    location: String,
-    page: String,
-    status: String,
-
-    // for history
-    back: Vec<String>,
-    forward: Vec<String>,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    tabs: Vec<Tab>,
+    current_tab: String,
+    current_location: String,
+    current_page: String,
+    current_status: String,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            location: "https://example.com".to_owned(),
-            page: "".to_owned(),
-            status: "Loaded".to_owned(),
-            back: Vec::new().to_owned(),
-            forward: Vec::new().to_owned(),
-            value: 2.7,
+            tabs: vec![Tab {
+                label: "Home".to_owned(),
+                location: "https://example.com".to_owned(),
+                status: "Loaded".to_owned(),
+                contents: "".to_owned(),
+                back: Vec::new().to_owned(),
+                forward: Vec::new().to_owned(),
+            }],
+            current_tab: "Home".to_owned(),
+            current_location: "https://example.com".to_owned(),
+            current_status: "Loaded".to_owned(),
+
+
+            // currently loaded page to display in main area
+            current_page: "".to_owned(),
+
         }
     }
 }
-
 impl TemplateApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -53,37 +71,24 @@ impl TemplateApp {
         Default::default()
     }
 
-    pub fn navigate(&mut self) -> String {
-        self.status = "Loading...".to_string();
-        println!("URL to navigate to: {}", self.location);
-        //let resp = reqwest::blocking::get(location).unwrap().text();
-        let resp = reqwest::blocking::get(&self.location)
-            .and_then(|r| r.text())
-            .map_err(|e| e.to_string());
-        //println!("{:#?}", resp);
-        self.status = "Loaded".to_string();
-        return resp.unwrap_or_else(|_| "Failed to load page".to_string());
-    }
-
-    pub fn visit(&mut self) -> String {
-        self.status = "Loading...".to_string();
-        println!("Visiting URL: {}", self.location);
-        //let resp = reqwest::blocking::get(location).unwrap().text();
-        let resp = reqwest::blocking::get(&self.location)
-            .and_then(|r| r.text())
-            .map_err(|e| e.to_string());
-        self.status = "Loaded".to_string();
-        // update the current page in history
-        println!("self back: {:?}", self.back);
-        println!("self forward: {:?}", self.forward);
-        return resp.unwrap_or_else(|_| "Failed to load page".to_string());
-    }
 
     pub fn back(&mut self) -> Result<(), Box<dyn Error>> {
-        if let Some(last) = self.back.pop() {
-            self.forward.push(self.location.clone());
-            self.location = last.clone();
-            self.page = self.visit();
+        let mut tab_found = false;
+        for tab in &mut self.tabs {
+            if tab.label == self.current_tab {
+                if let Some(last) = tab.back.pop() {
+                    tab.forward.push(last.clone());
+                    self.current_location = last.clone();
+                    self.current_page = navigate(self.current_location.clone());
+                    tab.contents = self.current_page.clone();
+                    tab_found = true;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        if tab_found {
             Ok(())
         } else {
             Err("No back history".into())
@@ -91,13 +96,40 @@ impl TemplateApp {
     }
 
     pub fn forward(&mut self) -> Result<(), Box<dyn Error>> {
-        if let Some(next) = self.forward.pop() {
-            self.back.push(self.location.clone());
-            self.location = next.clone();
-            self.page = self.visit();
+        let mut tab_found = false;
+        for tab in &mut self.tabs {
+            if tab.label == self.current_tab {
+                if let Some(next) = tab.forward.pop() {
+                    tab.back.push(tab.location.clone());
+                    self.current_location = next.clone();
+                    self.current_page = navigate(self.current_location.clone());
+                    tab.contents = self.current_page.clone();
+                    tab_found = true;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        if tab_found {
             Ok(())
         } else {
             Err("No forward history".into())
+        }
+    }
+
+    pub fn go(&mut self) {
+        for tab in &mut self.tabs {
+            if tab.label == self.current_tab {
+                tab.back.push(tab.location.clone());
+                tab.forward.clear(); // clear forward history
+                tab.location = self.current_location.clone();
+                tab.contents = navigate(self.current_location.clone());
+                self.current_page = tab.contents.clone();
+                tab.label = get_heading(tab.contents.clone());
+                self.current_tab = tab.label.clone();
+                return;
+            }
         }
     }
 }
@@ -122,6 +154,7 @@ impl eframe::App for TemplateApp {
                 if !is_web {
                     ui.menu_button(egui_material_icons::icons::ICON_MENU, |ui| {
                         egui::widgets::global_theme_preference_buttons(ui);
+                        powered_by_egui_and_eframe(ui);
                         if ui.button("Quit").clicked() {
                             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                         }
@@ -134,9 +167,9 @@ impl eframe::App for TemplateApp {
                     .clicked()
                     .then(|| {
                         if let Err(e) = self.back() {
-                            self.status = e.to_string();
+                            self.current_status = e.to_string();
                         } else {
-                            self.status = "Loaded".to_string();
+                            self.current_status = "Loaded".to_string();
                         }
                     });
                 ui.add_space(1.0);
@@ -145,9 +178,9 @@ impl eframe::App for TemplateApp {
                     .clicked()
                     .then(|| {
                         if let Err(e) = self.forward() {
-                            self.status = e.to_string();
+                            self.current_status = e.to_string();
                         } else {
-                            self.status = "Loaded".to_string();
+                            self.current_status = "Loaded".to_string();
                         }
                     });
                 ui.add_space(1.0);
@@ -157,26 +190,20 @@ impl eframe::App for TemplateApp {
 
                 let button_width = 25.0;
                 let text_edit_width = ui.available_width() - button_width;
-                let response = ui.add_sized([text_edit_width.max(0.0), 20.0], egui::TextEdit::singleline(&mut self.location));
+                let response = ui.add_sized([text_edit_width.max(0.0), 20.0], egui::TextEdit::singleline(&mut self.current_location));
                 if response.lost_focus() && response.ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
-                    self.status = "Loading...".to_string();
-                    // update the current page in history
-                    self.back.push(self.location.clone());
-                    // clear the forward history
-                    self.forward.clear();
-                    self.page = self.navigate();
+                    self.current_status = "Loading...".to_string();
+                    self.go();
+                    self.current_status = "Loaded".to_string();
                 }
                 ui.add_space(1.0);
                 ui.button(egui_material_icons::icons::ICON_KEYBOARD_DOUBLE_ARROW_RIGHT)
                     .on_hover_text("Go")
                     .clicked()
                     .then(|| {
-                        self.status = "Loading...".to_string();
-                        // update the current page in history
-                        self.back.push(self.location.clone());
-                        // clear the forward history
-                        self.forward.clear();
-                        self.page = self.navigate();
+                        self.current_status = "Loading...".to_string();
+                        self.go();
+                        self.current_status = "Loaded".to_string();
                     });
             });
         });
@@ -184,7 +211,7 @@ impl eframe::App for TemplateApp {
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
             //powered_by_egui_and_eframe(ui);
             let mut status_display: String = "Status: ".to_owned();
-            let status: &str = self.status.as_str();
+            let status: &str = self.current_status.as_str();
             status_display.push_str(status);
             ui.label(status_display);
         });
@@ -192,16 +219,33 @@ impl eframe::App for TemplateApp {
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             // The side panel is often a good place for a navigation menu:
             ui.separator();
+            for tab in &self.tabs {
+                if ui.button(&tab.label).clicked() {
+                    self.current_location = tab.location.clone();
+                    self.current_page = tab.contents.clone();
+                    self.current_tab = tab.label.clone();
+                }
+            }
 
-            if ui.button("Home").clicked() {
-                self.status = "Loading...".to_string();
-                self.page = self.navigate();
+            if ui.button("+").clicked() {
+                let new_tab = Tab {
+                    label: "New Tab".to_owned(),
+                    location: "https://example.com".to_owned(),
+                    status: "Loaded".to_owned(),
+                    contents: "".to_owned(),
+                    back: Vec::new().to_owned(),
+                    forward: Vec::new().to_owned(),
+                };
+                self.tabs.push(new_tab.clone());
+                self.current_tab = new_tab.label.clone();
+                self.current_location = new_tab.location.clone();
+                self.current_page = new_tab.contents.clone();
             }
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
 
-            let binding = self.page.clone();
+            let binding = self.current_page.clone();
             let markdown = binding.as_str();
             let mut all_links: Vec<String> = Vec::new();
 
@@ -232,13 +276,10 @@ impl eframe::App for TemplateApp {
                 for link in all_links {
                     if cache.get_link_hook(&link) == Some(true) {
                         println!("Link was clicked {link}");
-                        self.back.push(self.location.clone());
-                        self.location = link.clone();
-                        self.status = "Loading...".to_string();
-                        // update the current page in history
-                        // clear the forward history
-                        self.forward.clear();
-                        self.page = self.navigate();
+                        self.current_location = link.clone();
+                        self.current_status = "Loading...".to_string();
+                        self.go();
+                        self.current_status = "Loaded".to_string();
                     }
                     //ui.hyperlink_to(link, link);
                 }
@@ -255,9 +296,43 @@ impl eframe::App for TemplateApp {
             //ui.separator();
             //ui.label("end");
         });
-
     }
+}
+fn navigate(location: String) -> String {
+    //self.current_status = "Loading...".to_string();
+    println!("URL to navigate to: {}", location);
+    //let resp = reqwest::blocking::get(location).unwrap().text();
+    let resp = reqwest::blocking::get(location)
+        .and_then(|r| r.text())
+        .map_err(|e| e.to_string());
+    //println!("{:#?}", resp);
+    //self.current_status = "Loaded".to_string();
+    return resp.unwrap_or_else(|_| "Failed to load page".to_string());
+}
 
+fn get_heading(contents: String) -> String {
+    let mut heading = String::new();
+    let mut in_heading = false;
+    let parser = Parser::new_ext(&contents, Options::empty());
+    for event in parser {
+        match event {
+            pulldown_cmark::Event::Start(pulldown_cmark::Tag::Heading { .. }) => {
+                in_heading = true;
+            }
+            pulldown_cmark::Event::End(pulldown_cmark::TagEnd::Heading(_)) => {
+                if in_heading {
+                    break;
+                }
+            }
+            pulldown_cmark::Event::Text(text) => {
+                if in_heading {
+                    heading.push_str(&text);
+                }
+            }
+            _ => {}
+        }
+    }
+    heading
 }
 
 
